b8d1b0c1a9baf364b05192f1976c214778d5ffe7
Satyaprakash jha <satyaprakashjha19@yahoo.com>
2022-12-03 21:42:42 +0530

    string function

---
+++ stringfunc.py
@@ -0,0 +1,21 @@
+# replace
+name = "geekyshows"
+old = "geeky"
+new = "new"
+sr = name.replace(old,new)
+print(name)
+print(sr)
+
+# split  #return list [by this ,]
+name = "hello - my - name - is - satyaprakash"
+str1 = name.split('-')
+print(str1)
+
+#join  Converts the elements of an iterable into a string
+# using separator
+
+name1 = ( 'hello', 'my', 'name', 'is', 'salman')
+str2 = "_".join(name1)
+print(name1)
+print(str2)
+
