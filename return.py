e599eaa3e06f942e1a5545230b98e0917ca6b19f
Satyaprakash jha <satyaprakashjha19@yahoo.com>
2023-01-03 23:15:55 +0530

    function

--- return.py
+++ return.py
@@ -1,7 +1,20 @@
+# return statement single value
 def add(y):
     x = 10
-    c = x + y
-    return c
-sum = add(20)
+    return x + y
 
+# calling a function
+
+sum = add(50)
 print(sum)
+
+# multiple value
+
+def add(y):
+    x = 10
+    a = x + y
+    b = y - x
+    return a , b
+
+sum, sub = add(20)
+print(sum,sub)
