488db25b078ecffcc01feb4749b59264a879416f
Satyaprakash jha <satyaprakashjha19@yahoo.com>
2023-01-11 23:34:11 +0530

    map

--- map.py
+++ map.py
@@ -8,3 +8,15 @@ print(result)
 print(type(result))
 for i in result:
     print(i)
+
+
+print("Using Lambda Function")
+a = [10,20,30,40,50]
+
+
+
+result = list(map(lambda n :n+2, a))
+print(result)
+print(type(result))
+for i in result:
+    print(i)
